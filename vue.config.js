5c1c58e8dc27674b9851a7f699c9f2a702848dce
Aric Masih <masiharic@seattleu.edu>
2019-06-07 10:08:00 -0700

    fixing config file

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
 const path = require('path');
 module.exports = {
-    configureWebpack: {
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'development') {
+            config.devtool = 'eval-source-map';
+            config.output.devtoolModuleFilenameTemplate = info =>
+      
+              info.resourcePath.match(/\.vue$/) && !info.identifier.match(/type=script/) // this is change ✨
+              ?
+              `webpack-generated:///${info.resourcePath}?${info.hash}` :
+              `webpack-yourCode:///${info.resourcePath}`;
+      
+            config.output.devtoolFallbackModuleFilenameTemplate = 'webpack:///[resource-path]?[hash]';
+          }
         resolve: {
             //allow for @ or @src alias for src
             alias: require('./aliases.config').webpack
